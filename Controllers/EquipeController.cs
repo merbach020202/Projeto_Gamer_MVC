e400e1fa231d96b6dd7f8e005b3c15b6c2320645
merbach020202 <eduardo.brenn2006@gmail.com>
2023-05-31 10:50:48 -0300

    31/05

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoGamer_MVC.Infra;
+using ProjetoGamer_MVC.Models;
 
 namespace ProjetoGamer_MVC.Controllers
 {
@@ -24,6 +25,62 @@ namespace ProjetoGamer_MVC.Controllers
             //retorna a view de equipe (TELA)
             return View();
         }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //Instância de objeto equipe
+            Equipe novaEquipe = new Equipe();
+
+            //atribuição de valores recebidos do formulário
+            novaEquipe.Nome = form["Nome"].ToString();
+
+            //aqui estava chegando como string (não queremos assim)
+            // nova equipe.imagem = form["imagem"].ToString
+
+            //Início da lógica do upload de imagem
+            if (form.Files.Count > 0)
+            {
+
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                //guarda caminho completo até o caminho do arquivo(imagem - nome com extensão)
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem = "Padrão.Png";
+            }
+
+            //adiciona objeto na tabela do BD
+            c.Equipe.Add(novaEquipe);
+
+            //salva as alterações feitas no BD
+            c.SaveChanges();
+
+            //retorna para o local chamando a rota de listar(método index)
+            return LocalRedirect("~/Equipe/Listar");
+        }
+        //fim da lógica de upload
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
