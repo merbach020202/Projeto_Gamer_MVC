222cd1f71e96599bdd0a8f76bca8d85d7f8dbd7d
merbach020202 <eduardo.brenn2006@gmail.com>
2023-06-06 08:07:08 -0300

    06/06

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using ProjetoGamer_MVC.Infra;
 
 namespace ProjetoGamer_MVC.Controllers
