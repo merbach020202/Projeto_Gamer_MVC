7dd062f18346d3a06b631e3a7b9435e52e8381b2
merbach020202 <eduardo.brenn2006@gmail.com>
2023-06-12 10:00:37 -0300

    112/06

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -44,6 +44,19 @@ namespace ProjetoGamer_MVC.Controllers
             return LocalRedirect("~/Login/Login");
         }
 
+        [Route("Logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("UserName");
+            
+            return LocalRedirect("~/");
+        }
+
+
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
